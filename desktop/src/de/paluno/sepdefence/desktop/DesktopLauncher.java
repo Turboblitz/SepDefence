25f183e10f34181359828fdca8f71db312c6acba
Tobias-Braun <tobiasmarkusbraun@gmail.com>
2017-06-27 13:48:24 +0200

    Initial commit

---
+++ desktop/src/de/paluno/sepdefence/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.paluno.sepdefence.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.paluno.sepdefence.GameLoopHandler;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameLoopHandler(), config);
+	}
+}
